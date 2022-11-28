5450b825773350dd96d83cc0590aefea48fd8a69
Caleb Adewole Erioluwa <caleberioluwa@gmail.com>
2022-11-28 02:35:29 +0100

    update  bcrypt to `@v0.4.1`

--- lib/utils/bcrypt.ts
+++ lib/utils/bcrypt.ts
@@ -1,4 +1,4 @@
-import * as bcrypt from 'https://deno.land/x/bcrypt@v0.3.0/mod.ts';
+import * as bcrypt from 'https://deno.land/x/bcrypt@v0.4.1/mod.ts';
 
 export const bcryptEncrypt = async (
 	payload: string,
